70f413f73b515bbfbf52bac0744211241ac9b2df
Michael Johnsey <mjohnsey@gmail.com>
2019-07-01 17:28:11 -0500

    Deploy via hugo build

---
+++ docs/js/facts.js
@@ -0,0 +1,5 @@
+const facts = {};
+
+facts.birthdate = "2019-05-01";
+
+export default facts;
---
+++ docs/js/index.js
@@ -0,0 +1,27 @@
+import facts from "/js/facts.js";
+
+const relativeAgeId = "#realtiveAge";
+const birthdateId = "#birthdate";
+
+const setRelativeAge = age => {
+  $(relativeAgeId).text(age);
+};
+
+const setBirthdate = birthdate => {
+  $(birthdateId).text(`Born on ${birthdate.format("MMMM Do YYYY")}`);
+};
+
+const relativeAgeText = birthdate => {
+  const today = moment();
+  const weeks = today.diff(birthdate, "week");
+
+  let age = `${birthdate.fromNow(true)} old`;
+  if (weeks < 24) {
+    age = `${age} (${weeks} weeks)`;
+  }
+  return age;
+};
+
+const birthdate = moment(facts.birthdate);
+setBirthdate(birthdate);
+setRelativeAge(relativeAgeText(birthdate));

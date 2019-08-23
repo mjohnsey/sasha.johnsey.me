16cf9b3e0b48af9e8572858e95ec2092086dad29
Michael Johnsey <mjohnsey@gmail.com>
2019-08-23 10:25:37 -0500

    Add new weight

--- src/facts.ts
+++ src/facts.ts
@@ -7,5 +7,6 @@ const WeightMeasures: WeightMeasurement[] = [];
 WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-06-27", 2.3));
 WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-07-12", 3.06));
 WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-08-03", 4.3));
+WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-08-23", 6.13));
 
 export {Birthday, WeightMeasures};

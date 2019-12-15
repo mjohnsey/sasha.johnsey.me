68a1a2f9f0f145f81fd8fa869f36cb95d1ef8586
Michael Johnsey <mjohnsey@gmail.com>
2019-12-15 10:29:45 -0600

    Add vet visit weight for December

--- src/facts.ts
+++ src/facts.ts
@@ -9,5 +9,6 @@ WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-07-12", 3.06)
 WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-08-03", 4.3));
 WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-08-23", 6.13));
 WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-11-18", 8.4));
+WeightMeasures.push(WeightMeasurement.BuildWeightMeasurement("2019-12-13", 9.6));
 
 export {Birthday, WeightMeasures};

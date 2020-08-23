06753ba80300b574bb59d66b254d18353f5ae7fb
Michael Johnsey <mjohnsey@gmail.com>
2020-08-22 23:27:21 -0500

    Add weight chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,39 @@ import './App.scss';
 import moment from 'moment';
 import "moment-precise-range-plugin";
 import Iframe from 'react-iframe';
+import * as _ from 'lodash';
+import {Line} from 'react-chartjs-2';
+
+class Weight {
+  constructor(readonly date: moment.Moment, readonly weight: number) { }
+
+  public asChartData() {
+    return { x: this.date.format("YYYY-MM-DD"), y: this.weight };
+  }
+}
 
 type AppState = {
-  dob: string
+  dob: string,
+  weights: Weight[]
 }
+
 
 class App extends React.Component<{}, AppState> {
   constructor(props: Readonly<{}>) {
     super(props);
-    this.state = {dob: "2019-05-01"}
+    this.state = {
+      dob: "2019-05-01",
+      weights: [new Weight(moment("2019-06-27"), 2.3),
+      new Weight(moment("2019-07-12"), 3.06),
+      new Weight(moment("2019-08-03"), 4.3),
+      new Weight(moment("2019-08-23"), 6.13),
+      new Weight(moment("2019-11-18"), 8.4),
+      new Weight(moment("2019-12-13"), 9.6),
+      new Weight(moment("2020-01-03"), 9.3),
+      new Weight(moment("2020-06-09"), 10.10),
+      new Weight(moment("2020-07-03"), 11),
+      new Weight(moment("2020-08-21"), 11.3),],
+    }
   }
 
   birthdate() {
@@ -39,6 +63,57 @@ class App extends React.Component<{}, AppState> {
     return age;
   }
 
+  weightData() {
+    const weightChartData = _.map(this.state.weights, w => w.asChartData());
+    return weightChartData;
+  }
+
+  chartData() {
+    const data = {
+      datasets: [{
+        data: _.map(this.state.weights, (weight) => weight.asChartData()),
+        fill: false,
+        label: "Weight (lbs)",
+      }],
+    };
+    return data;
+  }
+
+  chartOptions() {
+    const mostRecentMeasure = _.maxBy(this.state.weights, (measure) => measure.date);
+    if (_.isUndefined(mostRecentMeasure)) {
+      throw new Error('problem figuring out most recent measure');
+    }
+    return {
+          legend: {
+            display: false,
+          },
+          responsive: true,
+          scales: {
+            xAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: "Date",
+              },
+              ticks: {},
+              type: "time",
+            }],
+            yAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: "Weight (lbs)",
+              },
+            }],
+          },
+          title: {
+            display: true,
+            text: `Sasha's Weight (Last: ${mostRecentMeasure.weight} on ${mostRecentMeasure.date.format("YYYY-MM-DD")})`,
+          },
+        }
+  }
+
   render() {
     return (
     <div className="App">
@@ -52,7 +127,7 @@ class App extends React.Component<{}, AppState> {
           <p>Born on {this.state.dob}</p>
           <p>{this.relativeAgeDate()}</p>
           <p>Food: <a href="https://smile.amazon.com/alimento-perros-adultos-HEALTH-NUTRITION/dp/B007PPTJLE/ref">Royal Canin Adult Small Breed</a></p>
-          <p>Weight Chart</p>
+            <Line data={this.chartData()} options={{...this.chartOptions(), ...{ maintainAspectRatio: true }}}/>
           <p>Sasha's Vet: <a href="https://thepethospitals.com/vets/lee-ann-newman">Lee Ann Newman, DVM</a></p>
           <div className="iframe">
             <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13059.689735583437!2d-89.75178085424798!3d35.08367076131394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x54b0ac735fed867e!2sThe+Pet+Hospitals!5e0!3m2!1sen!2sus!4v1564891889806!5m2!1sen!2sus" 
@@ -64,8 +139,9 @@ class App extends React.Component<{}, AppState> {
             />
           </div>
       </main>
+      <hr />
       <footer>
-      © 2020 Michael Johnsey
+      © {moment().format('YYYY')} Michael Johnsey
       </footer>
     </div>
     )
